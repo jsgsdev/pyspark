6c27a40c39292b9590ccac0e4aee38fb4868ef12
Jhon Steven Saavedra <jhon.giron@woombatcg.com>
2022-01-11 08:46:33 -0500

    commit

--- spark.py
+++ spark.py
@@ -14,7 +14,7 @@ textFile.first() # Saca la primera linea
 segunda = textFile.filter(lambda linea: 'segunda' in linea)
 segunda.collect() """
 
-lista = [1,2,3,4,5,6,7,8,9,10]
+""" lista = [1,2,3,4,5,6,7,8,9,10]
 rdd = sc.parallelize(lista)
 rdd.collect()
 
@@ -61,3 +61,57 @@ rdda = sc.parallelize([1,2,3])
 rddb = sc.parallelize([4,5,6])
 rddu = rdda.union(rddb)
 rddu.collect()
+
+# Intersection 
+new_rdda = sc.parallelize([1,2,3,4])
+new_rddb = sc.parallelize([3,4,5,6])
+rddi = new_rdda.intersection(new_rddb)
+rddi.collect()
+
+# Subtract devuelve los datos de a menos lo de b
+rdds = new_rdda.subtract(new_rddb)
+rdds.collect()
+
+# Cartesian producto cartesiano - operacion costosa
+rddc = new_rdda.cartesian(new_rddb)
+rddc.collect()
+
+ """
+rdd = sc.parallelize(range(1,6))
+rdd.collect()
+
+# Alternativa a collect
+# take  
+# takeWithSample
+# top 
+# takeOrdered
+
+rdd.take(3)
+rdd.takeSample(False, 3)
+rdd.top(4)
+rdd.takeOrdered(3) # Orden ascendente o unsando una key
+
+# Reduce, Fold, Aggregate
+
+rdd.reduce(lambda x, y: x + y)
+
+from operator import add, sub, mul
+rdd.reduce(add)
+rdd.reduce(sub)
+rdd.reduce(mul)
+
+rdd.fold(0, add) # Fold crea el valor cero para la operacion en cada particion y luego al final otra vez
+rdd.glom().collect()
+rdd.fold(1, mul)
+
+
+
+
+
+
+
+
+
+
+
+
